49018c13e59ec6b347205fb918eed12fb22e9b82
om <ombhandwalkar38126@gmail.com>
2025-09-21 14:58:52 +0530

    Custom_Exception

--- AI_Anime_Recommender_using_Grafana_Cloud_Minikube_ChromaDB_Langchain/utils/custom_exception.py
+++ AI_Anime_Recommender_using_Grafana_Cloud_Minikube_ChromaDB_Langchain/utils/custom_exception.py
@@ -1,11 +1,13 @@
 import sys
 
+# This defines a new exception type that extends the Python's built in "Exception"
 class CustomException(Exception):
+     # This will initiate - human readable error message and original exception
     def __init__(self, message:str, error_details:Exception=None):
         self.error_message= self.get_detailed_error_message(message, error_details)
         super().__init__(self.error_message)
 
-    @staticmethod
+    @staticmethod # Will get most recent custom exception.
     def get_detailed_error_message(message, error_details):
         _, _, exc_tb= sys.exc_info()
         file_name= exc_tb.tb_frame.f_code.co_filename if exc_tb else "Unknown File"
