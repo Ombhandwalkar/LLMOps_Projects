cf3772cd256776bfe0f815d220a299bd1a7d495e
om <ombhandwalkar38126@gmail.com>
2025-09-28 16:10:05 +0530

    Groq_client

---
+++ Study_AI_using_Jenkins_AgroCD_Kubernetes/src/llm/groq_client.py
@@ -0,0 +1,12 @@
+import os
+from src.config.settings import settings
+from langchain_groq import ChatGroq
+
+
+def get_groq_llm():
+    return ChatGroq(
+        model= settings.MODEL_NAME,
+        api_key= settings.GROQ_API_KEY,
+        temperature= settings.TEMPERATURE,
+        max_retries= settings.MAX_RETRIES
+    )
