770cf977a9a0440d969f18f8e073b08b9ef81aaa
om <ombhandwalkar38126@gmail.com>
2025-09-29 17:00:37 +0530

    Model

---
+++ Study_AI_using_Jenkins_AgroCD_Kubernetes/src/models/question_schemas.py
@@ -0,0 +1,26 @@
+from typing import List
+from pydantic import BaseModel, Field, field_validator
+
+class MCQQuestion(BaseModel):
+
+    question: str= Field(description='The question text')
+    options: List[str] = Field(description='List of 4 options')
+    correct_ans: str = Field(description='The correct answer for options')
+
+    @field_validator('question', pre=True)
+    def clean_question(cls, v):
+        if isinstance(v, dict):
+            return v.get('description', str(v))
+        return str(v)
+    
+
+class FillBlankQuestion(BaseModel):
+    question: str= Field(description="The question text with '___' for the blank.")
+    answer: str = Field(description='The correct word or phrase for the blank')
+
+    @field_validator('question', pre=True)
+    def clean_question(cls, v):
+        if isinstance(v, dict):
+            return v.get('description', str(v))
+        return str(v)
+    
